25a4e35621d037dc8ebee78f456faa5849732b7e
ApsaraWitharana <asachini095@gmail.com>
2024-04-27 10:28:06 +0530

    Time Out - set and interval - timeout -js

--- js/Timers.js
+++ js/Timers.js
@@ -9,10 +9,28 @@
 //     console.log('usng arrow funcion Hellow ..waiting timeperiade')
 // },3000)
 
-let timeout = setTimeout(function(){
-    console.log('Annonimes Hellow ..waiting timeperiade')
-},3000)
+// let timeout = setTimeout(function(){
+//     console.log('Annonimes Hellow ..waiting timeperiade')
+// },3000)
 
-clearTimeout(timeout)
+// clearTimeout(timeout)
 
 //2.setInterval
+// setInterval(callback,interval)
+//arrowfntion
+
+let interval=setInterval(() => {
+    console.log('arrow funcion -- Heyyyyy')
+}, 1000)
+
+clearInterval(interval)
+
+// second outsite eke funcion ek define kran ek
+let greet = ()=>{
+    console.log('hiii funcion define the outsite')
+}
+//funcion hoiste krnn ona nm funcion keyword ekt ekk define krann ona
+let interval2 = setInterval(greet,1000
+    
+);
+
